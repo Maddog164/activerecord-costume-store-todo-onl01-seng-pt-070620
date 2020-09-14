f6e8a6e917c50be778b0b83a3e821eb12214592b
Denise Henderson <denise@henderson3.us>
2020-09-14 19:48:50 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,7 @@ class CreateCostumeStores<ActiveRecord::Migration[5.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
+      t.string :location
       t.integer :number_of_costumes
       t.integer :number_of_employees
       t.boolean :still_in_business
